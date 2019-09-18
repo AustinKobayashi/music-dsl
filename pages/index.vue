1e95e858a649e9557cea560ac653b3f195a2999d
Austin Kobayashi <austinkobayashi@gmail.com>
2019-09-17 17:26:44 -0700

    first

---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="container">
+    <div id="editor">
+      <textarea id="input-area" v-model="user_input" @change="generate_music" />
+    </div>
+    <div id="osmdCanvas" />
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-undef */
+import '../static/opensheetmusicdisplay.min'
+import parser from '../plugins/parser'
+
+export default {
+  data() {
+    return {
+      openSheetMusicDisplay: undefined,
+      user_input: ''
+    }
+  },
+  mounted() {
+    this.openSheetMusicDisplay = new opensheetmusicdisplay.OpenSheetMusicDisplay('osmdCanvas', {
+      // set options here
+      backend: 'svg',
+      drawFromMeasureNumber: 1,
+      drawUpToMeasureNumber: Number.MAX_SAFE_INTEGER
+    })
+    this.openSheetMusicDisplay
+      .load(parser.parse(''))
+      .then(
+        function () {
+          window.osmd = this.openSheetMusicDisplay
+          this.openSheetMusicDisplay.render()
+        }
+      )
+  },
+  methods: {
+    generate_music() {
+      this.openSheetMusicDisplay
+        .load(parser.parse(this.user_input))
+        .then(
+          function () {
+            window.osmd = this.openSheetMusicDisplay
+            this.openSheetMusicDisplay.render()
+          }
+        )
+    }
+  }
+}
+</script>
+
+<style>
+#container {
+  display: flex;
+  min-height: 100vh;
+}
+
+#input-area {
+  min-height: 100vh;
+  width: 30vw;
+  font-size: 1.5rem;
+}
+
+#osmdCanvas {
+  width: 70vw;
+}
+</style>
---
+++ plugins/parser.ts
@@ -0,0 +1,50 @@
+const parser = {
+
+  // !!!!!!!!!!!!!!!!!!!!!!!!
+  // parse method stub
+  // !!!!!!!!!!!!!!!!!!!!!!!!
+  parse(user_input) {
+
+    // This just returns a basic version of the xml
+    // TODO: add parsing functionality
+    return '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n' +
+      '<!DOCTYPE score-partwise PUBLIC\n' +
+      '    "-//Recordare//DTD MusicXML 3.1 Partwise//EN"\n' +
+      '    "http://www.musicxml.org/dtds/partwise.dtd">\n' +
+      '<score-partwise version="3.1">\n' +
+      '  <part-list>\n' +
+      '    <score-part id="P1">\n' +
+      '      <part-name>Music</part-name>\n' +
+      '    </score-part>\n' +
+      '  </part-list>\n' +
+      '  <part id="P1">\n' +
+      '    <measure number="1">\n' +
+      '      <attributes>\n' +
+      '        <divisions>1</divisions>\n' +
+      '        <key>\n' +
+      '          <fifths>0</fifths>\n' +
+      '        </key>\n' +
+      '        <time>\n' +
+      '          <beats>4</beats>\n' +
+      '          <beat-type>4</beat-type>\n' +
+      '        </time>\n' +
+      '        <clef>\n' +
+      '          <sign>G</sign>\n' +
+      '          <line>2</line>\n' +
+      '        </clef>\n' +
+      '      </attributes>\n' +
+      '      <note>\n' +
+      '        <pitch>\n' +
+      '          <step>C</step>\n' +
+      '          <octave>4</octave>\n' +
+      '        </pitch>\n' +
+      '        <duration>4</duration>\n' +
+      '        <type>whole</type>\n' +
+      '      </note>\n' +
+      '    </measure>\n' +
+      '  </part>\n' +
+      '</score-partwise>\n';
+  }
+};
+
+export default parser;
